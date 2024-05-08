05191cbb811355bd746f46358e25c343d5d7ec0d
Mfordantel <mfordantel@gmail.com>
2024-05-08 18:21:10 +0500

    1

--- src/client.py
+++ src/client.py
@@ -3,17 +3,14 @@ import sys
 from functools import partial
 from random import randint
 from typing import AsyncGenerator
-
 import loguru
 import socketio
 import pygame
 from pygame.locals import *
-
 from src.proto import EVENT_XY, TASK_PERIOD, StructXY, clinet_config
 from src.utils import Task, enable_logger, get_task_queue, init_logger
 
 rand_int = partial(randint, -10000, 10000)
-
 
 class joystick_handler(object):
     def __init__(self, id):
@@ -22,7 +19,6 @@ class joystick_handler(object):
         self.name = self.joy.get_name()
         self.joy.init()
         self.numaxes = min(4, self.joy.get_numaxes())
-
         self.axes = [0.0] * self.numaxes
 
     def update(self):
@@ -30,15 +26,12 @@ class joystick_handler(object):
         for i in range(self.numaxes):
             self.axes[i] = round(self.joy.get_axis(i), 3)
 
-
 async def get_xy_data() -> StructXY:
     pygame.init()
     joy = joystick_handler(0)
     joy.update()
-    roll, pitch, throttle, yaw = [int((axis + 1) * 2047 / 2) for axis in joy.axes]
-    return StructXY(roll, pitch, throttle, yaw)
-
-
+    roll, pitch, throttle, yaw = [(axis + 1) * 818 + 173 for axis in joy.axes]
+    return StructXY(int(roll), int(pitch), int(throttle), int(yaw))
 
 async def xy_task_generator():
     task_queue = get_task_queue()
@@ -49,7 +42,6 @@ async def xy_task_generator():
         task_queue.put(Task(EVENT_XY, send_data))
         await asyncio.sleep(TASK_PERIOD)
 
-
 async def get_task() -> AsyncGenerator[Task, None]:
     task_queue = get_task_queue()
     while True:
@@ -58,7 +50,6 @@ async def get_task() -> AsyncGenerator[Task, None]:
             await asyncio.sleep(TASK_PERIOD)
             continue
         yield data
-
 
 async def manage_forever():
     config = clinet_config()
@@ -69,7 +60,6 @@ async def manage_forever():
         await sio.connect(**config)
         async for task in get_task():
             await sio.emit(task.event, task.data)
-
 
 async def main_client():
     while True:
@@ -83,16 +73,12 @@ async def main_client():
         finally:
             await asyncio.sleep(2)
 
-
 def main():
     init_logger("DEBUG", enable_logger=enable_logger())
-
     async def _main():
         asyncio.create_task(xy_task_generator())
         await main_client()
-
     asyncio.run(_main())
-
 
 if __name__ == "__main__":
     main()
